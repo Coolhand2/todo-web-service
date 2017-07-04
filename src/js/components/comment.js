81a4849f08a16aa531064ea8ad1b2a286f21e678
Mike Sherwood <coolhand2@gmail.com>
2017-07-03 21:09:41 -0600

    That hopeful thing called "comment".

---
+++ src/js/components/comment.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import User from './user';
+import * as details from '../util/UserDetails';
+
+export default class Comment extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <User display={details.USER_DETAIL_DISPLAY_COMMENT} username={this.props.username} />
+                <div className="col-xs-10 well comment-body"  dangerouslySetInnerHTML={{__html: this.props.comment}} />
+            </div>
+        );        
+    }
+}

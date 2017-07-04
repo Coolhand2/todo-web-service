15194446062078a8ab37423eba884888b725efe1
Mike Sherwood <coolhand2@gmail.com>
2017-07-03 18:33:02 -0600

    Adding homepage and basic application

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Screen from './components/screen';
+
+ReactDOM.render(
+    <Screen />, 
+    document.getElementById('screen')
+);

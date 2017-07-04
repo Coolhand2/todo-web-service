d4cb05af51fd75dc72b3272ad588fdce09cd9f87
Mike Sherwood <coolhand2@gmail.com>
2017-07-03 21:43:59 -0600

    Latest loss to the todo urgency update. Don't need these consts anymore.

--- src/js/util/urgencies.js
+++
@@ -1,4 +0,0 @@
-export const UNKNOWN = {value: 9, display: 'UNKNOWN'};
-export const HIGH = {value: 7, display: 'HIGH'};
-export const MEDIUM = {value: 5, display: 'MEDIUM'};
-export const LOW = {value:3, display: 'LOW'};
